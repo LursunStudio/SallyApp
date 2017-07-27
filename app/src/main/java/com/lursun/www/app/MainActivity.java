0996e6de49ba52e48eaae5d62c7664ed37a6b9ee
aa1191110tw <aa1191110tw@gamil.com>
2017-07-27 15:11:10 +0800

    美編BY阿賢

--- app/src/main/java/com/lursun/www/app/MainActivity.java
+++ app/src/main/java/com/lursun/www/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.lursun.www.app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         button01= (Button)findViewById(R.id.button);
 
@@ -36,4 +39,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
